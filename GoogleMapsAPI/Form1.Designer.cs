01e7981fa92fc5276d9403cf3e70d077db8c16d1
Gregory Kan <kan.gregory@gmail.com>
2015-07-21 17:12:52 +1200

    Simple google map search working

---
+++ GoogleMapsAPI/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace GoogleMapsAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txt_City = new System.Windows.Forms.TextBox();
+            this.txt_County = new System.Windows.Forms.TextBox();
+            this.txtbox_Street = new System.Windows.Forms.TextBox();
+            this.txt_Postcode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.search_data = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.search_data);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_Postcode);
+            this.splitContainer1.Panel1.Controls.Add(this.txtbox_Street);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_County);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_City);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(533, 429);
+            this.splitContainer1.SplitterDistance = 177;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(352, 429);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // txt_City
+            // 
+            this.txt_City.Location = new System.Drawing.Point(69, 91);
+            this.txt_City.Name = "txt_City";
+            this.txt_City.Size = new System.Drawing.Size(100, 20);
+            this.txt_City.TabIndex = 0;
+            // 
+            // txt_County
+            // 
+            this.txt_County.Location = new System.Drawing.Point(69, 117);
+            this.txt_County.Name = "txt_County";
+            this.txt_County.Size = new System.Drawing.Size(100, 20);
+            this.txt_County.TabIndex = 1;
+            // 
+            // txtbox_Street
+            // 
+            this.txtbox_Street.Location = new System.Drawing.Point(69, 65);
+            this.txtbox_Street.Name = "txtbox_Street";
+            this.txtbox_Street.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_Street.TabIndex = 2;
+            // 
+            // txt_Postcode
+            // 
+            this.txt_Postcode.Location = new System.Drawing.Point(69, 143);
+            this.txt_Postcode.Name = "txt_Postcode";
+            this.txt_Postcode.Size = new System.Drawing.Size(100, 20);
+            this.txt_Postcode.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Street";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "County";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PostCode";
+            // 
+            // search_data
+            // 
+            this.search_data.Location = new System.Drawing.Point(82, 180);
+            this.search_data.Name = "search_data";
+            this.search_data.Size = new System.Drawing.Size(75, 23);
+            this.search_data.TabIndex = 8;
+            this.search_data.Text = "Search";
+            this.search_data.UseVisualStyleBackColor = true;
+            this.search_data.Click += new System.EventHandler(this.search_data_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 429);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Postcode;
+        private System.Windows.Forms.TextBox txtbox_Street;
+        private System.Windows.Forms.TextBox txt_County;
+        private System.Windows.Forms.TextBox txt_City;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button search_data;
+    }
+}
+
---
+++ GoogleMapsAPI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleMapsAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void search_data_Click(object sender, EventArgs e)
+        {
+            string street = txtbox_Street.Text;
+            string city = txt_City.Text;
+            string County = txt_County.Text;
+            string Postcode = txt_Postcode.Text;
+
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("https://maps.google.com/maps?q=");
+
+
+                if (street != string.Empty)
+                {
+                    queryaddress.Append(street+"," + "+");
+                }
+
+                if (city != string.Empty)
+                {
+                    queryaddress.Append(city + "," + "+");
+                }
+
+                if (County != string.Empty)
+                {
+                    queryaddress.Append(County + "," + "+");
+                }
+
+                if (Postcode != string.Empty)
+                {
+                    queryaddress.Append(Postcode + "," + "+");
+                }
+
+                webBrowser1.Navigate(queryaddress.ToString());
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+    }
+}
